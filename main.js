e0f3d6429ba7034becf27298321781d40debfd52
HangaWilfried <wilfriedhanga5@gmail.com>
2021-07-25 13:41:51 +0100

    correction de la mauvaise fonctionalite de l'ancien fichier main.js

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 var information = [];
 var mesInputs = document.getElementsByTagName('input');
 var user ={
-    nom: `${mesInputs[0].value} ${mesInputs[1].value}`,
-    age: `${mesInputs[2].value}`,
-    sexe: `${mesInputs[3].value}`
+    "nom" : `${mesInputs[0].value}`,
+    "prenom" : `${mesInputs[1].value}`,
+    "age": `${mesInputs[2].value}`,
+    "sexe": `${mesInputs[3].value}`
 };
-var complete = [user.nom, user.age, user.sexe];
-var confirm = document.getElementById('register');
 
-
-for(let i=0; i<mesInputs.length; i++){
-    mesInputs[i].addEventListener('blur', function(){
-        information.push(this.value);
-    });
+complete = [];
+for(const [key, value] of Object.entries(user)){
+    complete.push(`${key} : ${value}`);
 }
 
+var confirm = document.getElementById('register');
 
 function stockage(){
+    for(let i=0; i<mesInputs.length; i++){
+        information.push(mesInputs[i].value);
+    }    
     var collecteurid = document.getElementById('collecteur');
     for(var i=0; i< information.length; i++){
         var paragraphe = document.createElement('p'),
         pref = document.createElement('pre');
-        pref.textContent = `${complete[i]}:${information[i]}`;
+        pref.textContent = `${complete[i]}`;
         paragraphe.appendChild(pref);
         collecteurid.appendChild(paragraphe);
     }
